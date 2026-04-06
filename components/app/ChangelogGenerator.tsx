f619a1811142cabbb6df9fdaab838d9ec5535be7
rosethornbush <31735267+rosethornbush@users.noreply.github.com>
2026-04-06 06:54:06 -0700

    fix: have calendar reset on select

--- components/app/ChangelogGenerator.tsx
+++ components/app/ChangelogGenerator.tsx
@@ -225,6 +225,7 @@ export function ChangelogGenerator() {
                         {(dates) => (
                           <Calendar
                             mode="range"
+                            resetOnSelect
                             defaultMonth={TODAY}
                             selected={dates}
                             disabled={{ after: TODAY }}
